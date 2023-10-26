e701244b56915c70c259a99169dd0a35509218aa
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-10-26 16:33:12 +0200

    rename

--- astroai/models/cnn.py
+++ astroai/models/cnn.py
@@ -10,7 +10,7 @@ import argparse
 import numpy as np
 import tensorflow as tf
 from datetime import datetime
-from os.path import join
+from os.path import join, isfile
 from astroai.tools.utils import load_yaml_conf, split_dataset
 TF_CPP_MIN_LOG_LEVEL="1"
 
@@ -19,7 +19,7 @@ def create_cnn_01():
     model.add(tf.keras.layers.Conv2D(2, (25, 25), activation='relu', input_shape=(250, 250, 1), name='conv2d_1'))
     model.add(tf.keras.layers.MaxPooling2D((20, 20), name='maxpool2d_1'))
     model.add(tf.keras.layers.Conv2D(2, (10, 10), activation='relu', name='conv2d_2'))
-    model.add(tf.keras.layers.MaxPooling2D((20, 20), name='maxpool2d_2'))
+    model.add(tf.keras.layers.MaxPooling2D((10, 10), name='maxpool2d_2'))
     model.add(tf.keras.layers.Conv2D(2, (5, 5), activation='relu', name='conv2d_3'))
     model.add(tf.keras.layers.Dropout(0.2), name='drop_1')
     # add dense layer
@@ -32,7 +32,7 @@ def create_cnn_01():
 def compile_and_fit(model, train_ds, test_ds, batch_sz=32, epochs=25, learning=0.001, shuffle=True, logdate=True):
     logdir = join("logs", "cnn-v01-detect") 
     if logdate:
-        logdir += datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+        logdir += datetime.now().strftime("%Y%m%dT%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning), 
               loss=tf.keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
@@ -43,10 +43,13 @@ def compile_and_fit(model, train_ds, test_ds, batch_sz=32, epochs=25, learning=0
 def main(configuration):
     conf = load_yaml_conf(configuration)
     exposure = conf['preprocess']['time_stop'] - conf['preprocess']['time_start']
-    src_sz, bkg_sz = conf['preprocess']['src_sample'], conf['preprocess']['bkg_sample']
-    filename = join(conf['preprocess']['directory'], f"datasets_{exposure}s_{src_sz}src_{bkg_sz}bkg.npy") 
-    #filename example: /data01/homes/dipiano/E4/datasets_50s_1000src_1000bkg.npy
-    ds = np.load(filename, allow_pickle=True, encoding='latin1', fix_imports=True).flat[0]
+    sample = conf['preprocess']['sample']
+    smoothing = conf['preprocess']['smoothing']
+    filename = join(conf['preprocess']['directory'], f"ds_{exposure}s_{smoothing}sgm_{sample}sz.npy") 
+    if isfile(filename):
+        ds = np.load(filename, allow_pickle=True, encoding='latin1', fix_imports=True).flat[0]
+    else:
+        raise FileNotFoundError(filename)
     train_data, train_labels, test_data, test_labels = split_dataset(ds, split=conf['detection']['split'], reshape=conf['detection']['reshape'], binning=conf['preprocess']['binning'])
 
     # create TF dataset
