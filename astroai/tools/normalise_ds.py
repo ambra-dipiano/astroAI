60c779ab69918da37e2c13475a30b8aeaa73e691
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-10-26 15:41:25 +0200

    normalise dataset of maps

---
+++ astroai/tools/normalise_ds.py
@@ -0,0 +1,29 @@
+# *****************************************************************************
+# Copyright (C) 2023 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# Authors:
+# Ambra Di Piano <ambra.dipiano@inaf.it>
+# *****************************************************************************
+
+import argparse
+from os.path import join
+from astroai.tools.utils import load_yaml_conf, get_and_normalise_dataset
+
+def main(configuration):
+    conf = load_yaml_conf(configuration)
+    conf = conf['preprocess']
+    ds_path = join(conf['directory'])
+    if conf['preprocess']['min_norm'] is not None and conf['preprocess']['max_norm'] is not None:
+        norm = True
+    else:
+        norm = False
+    # create image dataset
+    ds = get_and_normalise_dataset(ds_path=ds_path, sample=conf['sample'], save=True, output=conf['directory'], normalise=norm, min_max_norm=[conf['preprocess']['min_norm'], conf['preprocess']['max_norm']])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-f', '--configuration', type=str, required=True, help="path to the configuration file")
+    args = parser.parse_args()
+
+    main(args.configuration)
