0ecded46b5123af526271653a72343901fca85f0
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-11-06 17:59:32 +0100

    autoencoder cleaner

--- astroai/models/cnn.py
+++ astroai/models/cnn.py
@@ -14,8 +14,42 @@ from os.path import join, isfile
 from astroai.tools.utils import load_yaml_conf, split_dataset
 TF_CPP_MIN_LOG_LEVEL="1"
 
-def create_cnn_binary_classifier(binning):
-    model = tf.keras.models.Sequential()
+def create_bkg_cleaner(binning):
+    input_shape = tf.keras.Input(shape=(binning, binning, 1))
+    # encoder
+    x = tf.keras.layers.Conv2D(50, (5, 5), activation='relu', padding='same')(input_shape)
+    x = tf.keras.layers.MaxPooling2D((5, 5), padding='same')(x)
+    x = tf.keras.layers.Conv2D(50, (5, 5), activation='relu', padding='same')(x)
+    encoded = tf.keras.layers.MaxPooling2D((5, 5), padding='same')(x)
+
+    # decoder
+    x = tf.keras.layers.Conv2D(50, (5, 5), activation='relu', padding='same')(encoded)
+    x = tf.keras.layers.UpSampling2D((5, 5))(x)
+    x = tf.keras.layers.Conv2D(50, (5, 5), activation='relu', padding='same')(x)
+    x = tf.keras.layers.UpSampling2D((5, 5))(x)
+    decoded = tf.keras.layers.Conv2D(1, (5, 5), activation='sigmoid', padding='same')(x)
+
+    # autoencoder
+    autoencoder = tf.keras.Model(input_shape, decoded)
+    autoencoder.summary()
+    return autoencoder
+
+def compile_and_fit_bkg_cleaner(model, train_noisy, train_clean, test_noisy, test_clean, batch_sz=32, epochs=25, learning=0.001, shuffle=True, logdate=True):
+    # tensorboard
+    if logdate:
+        logdir += datetime.now().strftime("%Y%m%dT%H%M%S")
+    logdir = join("logs", "cnn-v01-clean" + datetime.now().strftime("%Y%m%dT%H%M%S"))
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
+    # compile
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning),  
+                  loss=tf.keras.losses.binary_crossentropy, metrics=['accuracy'])
+    # fit
+    history = model.fit(train_noisy, train_clean, epochs=epochs, batch_size=batch_sz, shuffle=shuffle,
+                    validation_data=(test_noisy, test_clean),
+                    callbacks=[tensorboard_callback])
+    return history
+
+def create_binary_classifier(binning):
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Conv2D(2, (5, 5), activation='relu', input_shape=(binning, binning, 1), name='conv2d_1'))
     model.add(tf.keras.layers.MaxPooling2D((2, 2), name='maxpool2d_1'))
@@ -31,14 +65,17 @@ def create_cnn_binary_classifier(binning):
     return model
 
 def compile_and_fit_binary_classifier(model, train_ds, train_lb, test_ds, test_lb, batch_sz=32, epochs=25, learning=0.001, shuffle=True, logdate=True):
+    # tensorboard
     logdir = join("logs", "cnn-classify") 
     if logdate:
         logdir += datetime.now().strftime("%Y%m%dT%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning), 
-              loss=tf.keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
-    history = model.fit(train_ds, train_lb, batch_size=batch_sz, epochs=epochs, validation_data=(test_ds, test_lb), 
-            callbacks=[tensorboard_callback], shuffle=shuffle)
+    # compile
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning),  
+                  loss=tf.keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+    # fit
+    history = model.fit(train_ds, train_lb, batch_size=batch_sz, epochs=epochs, 
+                        validation_data=(test_ds, test_lb), callbacks=[tensorboard_callback], shuffle=shuffle)
     return history
 
 def main(configuration, mode):
@@ -57,7 +94,7 @@ def main(configuration, mode):
         # split dataset
         train_data, train_labels, test_data, test_labels = split_dataset(ds, split=conf['detection']['split'], reshape=conf['detection']['reshape'], binning=conf['preprocess']['binning'])
         # create model
-        model = create_cnn_binary_classifier(binning=conf['preprocess']['binning'])
+        model = create_binary_classifier(binning=conf['preprocess']['binning'])
         # compile and fit
         history = compile_and_fit_binary_classifier(model=model, train_ds=train_data, train_lb=train_labels, test_ds=test_data, test_lb=test_labels, batch_sz=conf['detection']['batch_sz'], epochs=conf['detection']['epochs'], shuffle=conf['detection']['shuffle'], learning=conf['detection']['learning'])
 
