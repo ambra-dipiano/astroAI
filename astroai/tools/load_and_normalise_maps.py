c51f121090b6616a352d2a928c01f36ee20add32
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-12-19 16:41:36 +0100

    load and smooth new dataset

---
+++ astroai/tools/load_and_normalise_maps.py
@@ -0,0 +1,30 @@
+# *****************************************************************************
+# Copyright (C) 2023 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# Authors:
+# Ambra Di Piano <ambra.dipiano@inaf.it>
+# *****************************************************************************
+
+import argparse
+from os.path import join
+from astroai.tools.utils import load_yaml_conf, process_dataset
+
+def main(configuration):
+    conf = load_yaml_conf(configuration)
+    conf = conf['preprocess']
+    if 'clean' in conf['mode']:
+        ds1_dataset_path = join(conf['directory'], 'noisy')
+        ds2_dataset_path = join(conf['directory'], 'clean')
+    else:
+        raise ValueError(f"Mode {conf['mode']} not valid")
+    trange = [conf['time_start'], conf['time_stop']]
+    # create image dataset
+    ds = process_dataset(ds1_dataset_path, ds2_dataset_path, trange=trange, smoothing=conf['smoothing'], binning=conf['binning'], sample=conf['sample'], save=True, output=conf['directory'], min_max_norm=conf['min_max_norm'], mode=conf['mode'], suffix=conf['suffix'], dl=4)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-f', '--configuration', type=str, required=True, help="path to the configuration file")
+    args = parser.parse_args()
+
+    main(args.configuration)
