f98196bcef3c1e24ccaaa1c16e7ce1ecebedfa67
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-12-21 16:37:08 +0100

    refactor to include regression

--- astroai/tools/preprocess_ds.py
+++ astroai/tools/preprocess_ds.py
@@ -8,22 +8,28 @@
 
 import argparse
 from os.path import join
-from astroai.tools.utils import load_yaml_conf, process_dataset
+from astroai.tools.utils import load_yaml_conf, process_dataset, process_regressor_dataset
 
 def main(configuration):
     conf = load_yaml_conf(configuration)
     conf = conf['preprocess']
+    trange = [conf['time_start'], conf['time_stop']]
+
     if 'detect' in conf['mode'] or 'class' in conf['mode']:
         ds1_dataset_path = join(conf['directory'], 'crab', 'sim')
         ds2_dataset_path = join(conf['directory'], 'background', 'sim')
+        ds = process_dataset(ds1_dataset_path, ds2_dataset_path, trange=trange, smoothing=conf['smoothing'], binning=conf['binning'], sample=conf['sample'], save=True, output=conf['directory'], norm_value=conf['norm_value'], mode=conf['mode'], suffix=conf['suffix'])
     elif 'clean' in conf['mode']:
-        ds1_dataset_path = join(conf['directory'], 'crab_only', 'sim')
-        ds2_dataset_path = join(conf['directory'], 'crab', 'sim')
+        ds1_dataset_path = join(conf['directory'], 'noisy')
+        ds2_dataset_path = join(conf['directory'], 'clean')
+        ds = process_dataset(ds1_dataset_path, ds2_dataset_path, trange=trange, smoothing=conf['smoothing'], binning=conf['binning'], sample=conf['sample'], save=True, output=conf['directory'], norm_value=conf['norm_value'], mode=conf['mode'], suffix=conf['suffix'])
+    elif 'loc' in conf['mode'] or 'regress' in conf['mode']:
+        ds_dataset_path = join(conf['directory'])
+        ds = process_regressor_dataset(ds_dataset_path, smoothing=conf['smoothing'], binning=conf['binning'], sample=conf['sample'], save=True, output=conf['directory'], norm_single_map=conf['norm_single_map'], stretch=conf['stretch'], norm_value=conf['norm_value'], suffix=conf['suffix'])
     else:
         raise ValueError(f"Mode {conf['mode']} not valid")
-    trange = [conf['time_start'], conf['time_stop']]
-    # create image dataset
-    ds = process_dataset(ds1_dataset_path, ds2_dataset_path, trange=trange, smoothing=conf['smoothing'], binning=conf['binning'], sample=conf['sample'], save=True, output=conf['directory'], min_max_norm=conf['min_max_norm'], mode=conf['mode'], suffix=conf['suffix'])
+
+  
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
