cb8b2d64e2c185c595427cf9cbbe1d6cd9d2b6c8
Ambra Di Piano <ambra.dipiano@inaf.it>
2026-02-11 12:52:40 +0100

    remove usr path to use expandvars instead, add usr and partition args for slurm jobs

--- astroai/models/slurmjobs.py
+++ astroai/models/slurmjobs.py
@@ -9,10 +9,10 @@
 
 import argparse
 from os import system, makedirs
-from os.path import abspath, join, dirname
+from os.path import abspath, join, dirname, expandvars
 from datetime import datetime
 
-def main(architecture, filename, mode, env):
+def main(architecture, filename, mode, env, usr, part):
     job_name = f'{architecture}_{mode}_{datetime.now().strftime("%Y%m%dT%H%M%S")}'
     # write bash
     makedirs(join(dirname(abspath(__file__)), 'slurms'), exist_ok=True)
@@ -20,7 +20,7 @@ def main(architecture, filename, mode, env):
     with open(sh_outname, 'w+') as f:
         f. write("#!/bin/bash\n")
         if env == 'venv':
-            f.write(f"\nsource /home/dipiano/venvs/astroai/bin/activate")
+            f.write(f"\nsource {expandvars('$HOME/venvs/astroai/bin/activate')}")
         elif env == 'conda':
             f.write(f"\nconda activate astroai")
         elif env == 'mamba':
@@ -34,8 +34,8 @@ def main(architecture, filename, mode, env):
         f.write('#!/bin/bash')
         f.write(f'\n\n#SBATCH --job-name={job_name}')
         f.write(f'\n#SBATCH --output={job_outlog}')
-        f.write('\n#SBATCH --account=dipiano')
-        f.write('\n#SBATCH --partition=large')
+        f.write(f'\n#SBATCH --account={usr}')
+        f.write(f'\n#SBATCH --partition={part}')
         f.write(f'\n\nexec sh {str(sh_outname)}\n')
 
     # sbatch job
@@ -47,8 +47,10 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--filename', type=str, required=True, help='Configuration YAML file')
     parser.add_argument('-m', '--mode', type=str, required=False, choices=['clean', 'classify', 'detect', 'loc', 'regression'], help='Scope of the model')
     parser.add_argument('-e', '--env', type=str, default='venv', choices=['venv', 'conda', 'mamba'], help='Virtual environtmet package')
+    parser.add_argument('-u', '--user', type=str, required=True, help='Account username for SLURM jobs')
+    parser.add_argument('-p', '--partition', type=str, default='large', help='Partition name for SLURM jobs')
     args = parser.parse_args()
 
-    main(args.architecture, args.filename, args.mode, args.env)
+    main(args.architecture, args.filename, args.mode, args.env, args.usr, args.partition)
 
 
